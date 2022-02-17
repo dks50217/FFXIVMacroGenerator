c2f72bd5cfd187866f4afcd43f6f323673f95eec
Michael_Chang <Michael_Chang@pegatroncorp.com>
2022-02-17 17:52:14 +0800

    1.add Element font 2.base drag ui

--- FFXIVMacroGenerator/FFXIVMacroGenerator/wwwroot/component/draggable.vue
+++ FFXIVMacroGenerator/FFXIVMacroGenerator/wwwroot/component/draggable.vue
@@ -1,33 +1,63 @@
 ﻿<template>
     <div>
         <el-row :gutter="20">
-            <div class="title">A列</div>
-            <draggable v-model="skillList" group="site" animation="300" dragClass="dragClass" ghostClass="ghostClass" chosenClass="chosenClass" @start="onStart" @end="onEnd">
-                <el-avatar v-for="(item,index) in skillList" shape="square" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg"></el-avatar>
-            </draggable>
+            <el-select v-model="selectJob" size="small">
+                <el-option v-for="item in jobOptions"
+                           :key="item.value"
+                           :label="item.label"
+                           :value="item.value">
+                </el-option>
+            </el-select>
         </el-row>
 
 
         <el-row :gutter="20">
-            <div class="title">B列</div>
-            <draggable v-model="settingList" group="site" animation="300" dragClass="dragClass" ghostClass="ghostClass" chosenClass="chosenClass" @start="onStart" @end="onEnd">
+            <el-tag effect="dark">A列</el-tag>
+        </el-row>
+
+        <el-row :gutter="20" class="skillArea">
+            <draggable class="list-group" v-model="skillList" group="site" animation="300" dragClass="dragClass" ghostClass="ghostClass" chosenClass="chosenClass" @start="onStart" @end="onEnd">
                 <transition-group>
-                    <el-card shadow="always" v-for="(item,index) in settingList" :key="index">
-                        {{item.name}}
-                    </el-card>
+                    <el-avatar v-for="(item,index) in skillList" shape="square" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" :key="index"></el-avatar>
                 </transition-group>
             </draggable>
         </el-row>
 
-        <el-row>
-            <el-col :span="6">
-                <raw-displayer :value="skillList" :title="'A'"></raw-displayer>
-            </el-col>
+        <el-divider></el-divider>
 
-            <el-col :span="6">
-                <raw-displayer :value="settingList" :title="'B'"></raw-displayer>
-            </el-col>
+        <el-row :gutter="20">
+            <el-tag type="success" effect="dark">B列</el-tag>
         </el-row>
+
+        <el-row :gutter="20" class="settingArea">
+            <draggable v-model="settingList" group="site" animation="300" dragClass="dragClass" ghostClass="ghostClass" chosenClass="chosenClass" @start="onStart" @end="onEnd">
+                <transition-group class="list-group">
+                    <el-avatar v-for="(item,index) in settingList" shape="square" src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" :key="index"></el-avatar>
+                </transition-group>
+            </draggable>
+        </el-row>
+
+        <el-divider></el-divider>
+
+        <el-row :gutter="20">
+            <el-input type="textarea"
+                      placeholder=""
+                      v-model="macroText"
+                      maxlength="30"
+                      show-word-limit>
+            </el-input>
+        </el-row>
+
+        <!--<el-row>
+        <el-col :span="6">
+            <raw-displayer :value="skillList" :title="'A'"></raw-displayer>
+        </el-col>
+
+        <el-col :span="6">
+            <raw-displayer :value="settingList" :title="'B'"></raw-displayer>
+        </el-col>
+    </el-row>-->
+
     </div>
 </template>
 
@@ -36,27 +66,49 @@
         data() {
             return {
                 drag: false,
+                jobOptions: [
+                    { label: 'JobA' , value: 1  },
+                    { label: 'JobB' , value: 2  },
+                    { label: 'JobC' , value: 3  }
+                ],
                 skillList: [
-                    { name: 'SampleA', image: '' },
-                    { name: 'SampleB', image: '' },
-                    { name: 'SampleC', image: '' },
-                    { name: 'SampleD', image: '' }
+                    { name: 'SkillA', image: '' },
+                    { name: 'SkillB', image: '' },
+                    { name: 'SkillC', image: '' },
+                    { name: 'SkillD', image: '' },
+                    { name: 'SkillA', image: '' },
+                    { name: 'SkillB', image: '' },
+                    { name: 'SkillC', image: '' },
+                    { name: 'SkillD', image: '' },
+                    { name: 'SkillA', image: '' },
+                    { name: 'SkillB', image: '' },
+                    { name: 'SkillC', image: '' },
+                    { name: 'SkillD', image: '' },
+                    { name: 'SkillA', image: '' },
+                    { name: 'SkillB', image: '' },
+                    { name: 'SkillC', image: '' },
+                    { name: 'SkillD', image: '' },
                 ],
                 settingList: [
-                    { id: 1, name: 'SampleE' },
-                    { id: 2, name: 'SampleF' },
-                    { id: 3, name: 'SampleG' },
-                    { id: 4, name: 'SampleH' }
-                ]
+                    //{ id: 1, name: 'SkillE' },
+                    //{ id: 2, name: 'SkillF' },
+                    //{ id: 3, name: 'SkillG' },
+                    //{ id: 4, name: 'SkillH' }
+                ],
+                macroText: '',
+                selectJob: ''
             };
         },
         methods: {
-            onStart() {
+            onStart(event) {
                 this.drag = true;
             },
             onEnd() {
                 this.drag = false;
             },
+            setMacroText() {
+
+            }
         },
         components: {
             'draggable': window['vuedraggable'],
@@ -66,4 +118,28 @@
 </script>
 
 <style scoped>
+    .el-avatar {
+        width: 80px;
+        height: 80px;
+        margin: 5px;
+        cursor: grab;
+    }
+
+    .skillArea, .settingArea {
+        overflow: auto;
+        white-space: nowrap;
+    }
+
+    .skillArea span, .settingArea span {
+        display: inline-block;
+    }
+
+    .el-row {
+        margin-bottom: 20px;
+    }
+
+    .list-group {
+        min-height: 100px;
+        min-width: 100px;
+    }
 </style>
